7a4736e337d4d1aab1e1a7b5acdbedc13e30c182
Miguel Alejandro <miguel.alejandro.ramirez.g@gmail.com>
2025-10-24 13:16:29 +0200

    Agregar ejercicios del libro 5

---
+++ bookV/Exercise01.java
@@ -0,0 +1,17 @@
+package bookV;
+
+public class Exercise01 {
+
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 10;
+
+        int result = add(a, b);
+        System.out.println("La suma de " + a + " y " + b + " es:" + result);
+
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ bookV/Exercise02.java
@@ -0,0 +1,11 @@
+package bookV;
+
+public class Exercise02 {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 12;
+        int c = 31;
+
+    }
+}
